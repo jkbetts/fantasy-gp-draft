24f81e9cc7199ffad87533c32e3fdfa1508d7438
Kevin Betts <jkevinbetts@gmail.com>
2020-02-25 12:13:32 -0600

    working on variable team count

---
+++ riders.js
@@ -0,0 +1,224 @@
+const EspA = {
+    name: ['# 41 Aleix Espargaro'],
+    team: 'Aprilia',
+    nation: 'Spain',
+    points: 63,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0,
+    pic: "images/riderpics/aleix.jpg",
+}
+const Zarco = {
+    name: ['#5 Johann Zarco'],
+    team: 'Reale Avintia Ducati',
+    nation: 'France',
+    points: 30,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0, 
+    pic: "images/riderpics/zarco.jpg",   
+}
+const Dovi = {
+    name: ['#04 Andrea Dovizioso'],
+    team: 'Ducati Factory',    
+    nation: 'Italy',
+    points: 269,
+    poles: 0,
+    wins: 2,
+    secondplaces: 3,
+    lapsinfirst: 33,
+    pic: "images/riderpics/dovi.jpg",
+}
+const Petrux = {
+    name: ['#9 Danilo Petrucci'],
+    team: 'Ducati Factory',
+    nation: 'Italy',
+    points: 176,
+    poles: 0,
+    wins: 1,
+    secondplaces: 0,
+    lapsinfirst: 14,
+    pic: "images/riderpics/petrux.jpg",
+}
+const Miller = {
+    name: ['#43 Jack Miller'],
+    team: 'Primac Ducati',
+    nation: 'Australia',
+    points: 165,
+    poles: 0,
+    wins: 2,
+    secondplaces: 0,
+    lapsinfirst: 2,
+    pic: "images/riderpics/miller.jpg",
+}
+const Pecco = {
+    name: ['#63 Francesco Bagnaia'],
+    team: 'Primac Ducati',
+    nation: 'Italy',
+    points: 54,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0,
+    pic: "images/riderpics/pecco.jpg",
+}
+const Nakagami = {
+    name: ['#30 Takaaki Nakagami'],
+    team: 'LCR Honda',
+    nation: 'Japan',
+    points: 74,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0,
+    pic: "images/riderpics/nakagami.jpg",
+}
+const Crutchlow = {
+    name: ['#35 Cal Crutchlow'],
+    team: 'LCR Honda',
+    nation: 'UK',
+    points: 133,
+    poles: 0,
+    wins: 0,
+    secondplaces: 1,
+    lapsinfirst: 6,
+    pic: "images/riderpics/crutchlow.jpg",
+}
+const MarA = {
+    name: ['#73 Alex Marquez'],
+    team: 'Repsol Honda',
+    nation: 'Spain',
+    points: 0,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0,
+    pic: "images/riderpics/alexm.jpg",
+}
+const MarM = {
+    name: ['#93 Marc Marquez'],
+    team: 'Repsol Honda',
+    nation: 'Spain',
+    points: 420,
+    poles: 10,
+    wins: 12,
+    secondplaces: 6,
+    lapsinfirst: 264,
+    pic: "images/riderpics/marc.jpg",
+}
+const Binder = {
+    name: ['#33 Brad Binder'],
+    team: 'KTM Factory',
+    nation: 'South Africa',
+    points: 0,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0,
+    pic: "images/riderpics/binder.jpg",
+}
+const Lecuona = {
+    name: ['#27 Iker Lecuona'],
+    team: 'KTM Factory',
+    nation: 'Spain',
+    points: 0,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0,
+    pic: "images/riderpics/lecuona.jpg",
+}
+const Oliveira = {
+    name: ['#88 Miguel Oliveira'],
+    team: 'Tech3 KTM',
+    nation: 'Portugal',
+    points: 33,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0,
+    pic: "images/riderpics/oliveira.jpg",
+}
+const Mir = {
+    name: ['#36 Joan Mir'],
+    team: 'Suzuki Factory',
+    nation: 'Spain',
+    points: 92,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0,
+    pic: "images/riderpics/mir.jpg",
+}
+const Rins = {
+    name: ['#42 Alex Rins'],
+    team: 'Suzuki Factory',
+    nation: 'Spain',
+    points: 205,
+    poles: 0,
+    wins: 2,
+    secondplaces: 1,
+    lapsinfirst: 12,
+    pic: "images/riderpics/rins.jpg",
+}
+const Vinales = {
+    name: ['#12 Maverick Vinales'],
+    team: 'Yamaha Factory',
+    nation: 'Spain',
+    points: 211,
+    poles: 3,
+    wins: 2,
+    secondplaces: 1,
+    lapsinfirst: 49,
+    pic: "images/riderpics/vinales.jpg",
+}
+const Rossi = {
+    name: ['#46 Valentino Rossi'],
+    team: 'Yamaha Factory',
+    nation: 'Italy',
+    points: 174,
+    poles: 0,
+    wins: 0,
+    secondplaces: 2,
+    lapsinfirst: 11,
+    pic: "images/riderpics/rossi.jpg",
+}
+const Morbidelli = {
+    name: ['#21 Franco Morbidelli'],
+    team: 'Petronas Yamaha',
+    nation: 'Italy',
+    points: 115,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0,
+    pic: "images/riderpics/morbidelli.jpg",
+}
+const Quartararo = {
+    name: ['#20 Fabio Quartararo'],
+    team: 'Petronas Yamaha',
+    nation: 'France',
+    points: 192,
+    poles: 6,
+    wins: 0,
+    secondplaces: 5,
+    lapsinfirst: 73,
+    pic: "images/riderpics/quartararo.jpg",
+}
+const Pol = {
+    name: ['#44 Pol Espargaro'],
+    team: 'KTM Factory',
+    nation: 'Spain',
+    points: 100,
+    poles: 0,
+    wins: 0,
+    secondplaces: 0,
+    lapsinfirst: 0,
+    pic: "images/riderpics/pol.jpg",
+}
+
+
+
+
--- scripts.js
+++ scripts.js
@@ -1,254 +1,31 @@
-const EspA = {
-    name: ['# 41 Aleix Espargaro'],
-    team: 'Aprilia',
-    nation: 'Spain',
-    points: 63,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0,
-    pic: "images/riderpics/aleix.jpg",
-}
-const Zarco = {
-    name: ['#5 Johann Zarco'],
-    team: 'Reale Avintia Ducati',
-    nation: 'France',
-    points: 30,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0, 
-    pic: "images/riderpics/zarco.jpg",   
-}
-const Dovi = {
-    name: ['#04 Andrea Dovizioso'],
-    team: 'Ducati Factory',    
-    nation: 'Italy',
-    points: 269,
-    poles: 0,
-    wins: 2,
-    secondplaces: 3,
-    lapsinfirst: 33,
-    pic: "images/riderpics/dovi.jpg",
-}
-const Petrux = {
-    name: ['#9 Danilo Petrucci'],
-    team: 'Ducati Factory',
-    nation: 'Italy',
-    points: 176,
-    poles: 0,
-    wins: 1,
-    secondplaces: 0,
-    lapsinfirst: 14,
-    pic: "images/riderpics/petrux.jpg",
-}
-const Miller = {
-    name: ['#43 Jack Miller'],
-    team: 'Primac Ducati',
-    nation: 'Australia',
-    points: 165,
-    poles: 0,
-    wins: 2,
-    secondplaces: 0,
-    lapsinfirst: 2,
-    pic: "images/riderpics/miller.jpg",
-}
-const Pecco = {
-    name: ['#63 Francesco Bagnaia'],
-    team: 'Primac Ducati',
-    nation: 'Italy',
-    points: 54,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0,
-    pic: "images/riderpics/pecco.jpg",
-}
-const Nakagami = {
-    name: ['#30 Takaaki Nakagami'],
-    team: 'LCR Honda',
-    nation: 'Japan',
-    points: 74,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0,
-    pic: "images/riderpics/nakagami.jpg",
-}
-const Crutchlow = {
-    name: ['#35 Cal Crutchlow'],
-    team: 'LCR Honda',
-    nation: 'UK',
-    points: 133,
-    poles: 0,
-    wins: 0,
-    secondplaces: 1,
-    lapsinfirst: 6,
-    pic: "images/riderpics/crutchlow.jpg",
-}
-const MarA = {
-    name: ['#73 Alex Marquez'],
-    team: 'Repsol Honda',
-    nation: 'Spain',
-    points: 0,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0,
-    pic: "images/riderpics/alexm.jpg",
-}
-const MarM = {
-    name: ['#93 Marc Marquez'],
-    team: 'Repsol Honda',
-    nation: 'Spain',
-    points: 420,
-    poles: 10,
-    wins: 12,
-    secondplaces: 6,
-    lapsinfirst: 264,
-    pic: "images/riderpics/marc.jpg",
-}
-const Binder = {
-    name: ['#33 Brad Binder'],
-    team: 'KTM Factory',
-    nation: 'South Africa',
-    points: 0,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0,
-    pic: "images/riderpics/binder.jpg",
-}
-const Lecuona = {
-    name: ['#27 Iker Lecuona'],
-    team: 'KTM Factory',
-    nation: 'Spain',
-    points: 0,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0,
-    pic: "images/riderpics/lecuona.jpg",
-}
-const Oliveira = {
-    name: ['#88 Miguel Oliveira'],
-    team: 'Tech3 KTM',
-    nation: 'Portugal',
-    points: 33,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0,
-    pic: "images/riderpics/oliveira.jpg",
-}
-const Mir = {
-    name: ['#36 Joan Mir'],
-    team: 'Suzuki Factory',
-    nation: 'Spain',
-    points: 92,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0,
-    pic: "images/riderpics/mir.jpg",
-}
-const Rins = {
-    name: ['#42 Alex Rins'],
-    team: 'Suzuki Factory',
-    nation: 'Spain',
-    points: 205,
-    poles: 0,
-    wins: 2,
-    secondplaces: 1,
-    lapsinfirst: 12,
-    pic: "images/riderpics/rins.jpg",
-}
-const Vinales = {
-    name: ['#12 Maverick Vinales'],
-    team: 'Yamaha Factory',
-    nation: 'Spain',
-    points: 211,
-    poles: 3,
-    wins: 2,
-    secondplaces: 1,
-    lapsinfirst: 49,
-    pic: "images/riderpics/vinales.jpg",
-}
-const Rossi = {
-    name: ['#46 Valentino Rossi'],
-    team: 'Yamaha Factory',
-    nation: 'Italy',
-    points: 174,
-    poles: 0,
-    wins: 0,
-    secondplaces: 2,
-    lapsinfirst: 11,
-    pic: "images/riderpics/rossi.jpg",
-}
-const Morbidelli = {
-    name: ['#21 Franco Morbidelli'],
-    team: 'Petronas Yamaha',
-    nation: 'Italy',
-    points: 115,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0,
-    pic: "images/riderpics/morbidelli.jpg",
-}
-const Quartararo = {
-    name: ['#20 Fabio Quartararo'],
-    team: 'Petronas Yamaha',
-    nation: 'France',
-    points: 192,
-    poles: 6,
-    wins: 0,
-    secondplaces: 5,
-    lapsinfirst: 73,
-    pic: "images/riderpics/quartararo.jpg",
-}
-const Pol = {
-    name: ['#44 Pol Espargaro'],
-    team: 'KTM Factory',
-    nation: 'Spain',
-    points: 100,
-    poles: 0,
-    wins: 0,
-    secondplaces: 0,
-    lapsinfirst: 0,
-    pic: "images/riderpics/pol.jpg",
-}
-
-
-
-
-
 let riders = [EspA, Zarco, Dovi, Petrux, Miller, Pecco, Nakagami, Crutchlow, MarA, MarM, Binder, Lecuona, Oliveira, Mir, Rins, Vinales, Rossi, Quartararo, Morbidelli, Pol];
-var riderDraft, teamNumString, firstDraft, currentDraft, alerts, riderCard;
+var riderDraft, teamID, firstDraft, currentDraft, alerts, riderCard;
 var teampoints = [0,0,0,0,0,0];
-var teamNumInt = 1;
+var teamNumInt = 0;
 let round = 1;
 
 
 $("#addNames").click(createTeam);
 
-function createTeam(){  
-    if (teamNumInt < 5){
-        teamNumString = "team" + teamNumInt;
+function createTeam(){
+    if (teamNumInt < 4){
+        teamID = "team" + teamNumInt;
         
         let newTeam = document.createElement("div");
-        newTeam.id = teamNumString;
-        newTeam.className = "teams";
-        document.body.appendChild(newTeam);
-        addTeamName(teamNumString);
+        let teamContainer = document.getElementById("team-container");
+        newTeam.id = teamID;
+        newTeam.className = "team";
+        teamContainer.appendChild(newTeam);
+        addTeamName(teamID);
 
         let pointsTotal = document.createElement('div');
         pointsTotal.id = "teampoints"+teamNumInt;
         pointsTotal.innerHTML = "";
         newTeam.appendChild(pointsTotal);
+
     }
     teamNumInt++;
-    if(teamNumInt == 5){
+    if(teamNumInt == 4){
         $("#user-create-container").hide();
         let rosterContainer = document.createElement("div");
         document.body.appendChild(rosterContainer);
@@ -258,14 +35,13 @@ function createTeam(){
         $("#start-draft").click(function(){startDraft();});
     }
 }
-function addTeamName(teamNumString){
+function addTeamName(teamID){
     let teamName = document.createElement("div");
-    let teamBox = document.getElementById(teamNumString);
-    teamName.id = "team-title"+teamNumInt;
+    let teamBox = document.getElementById(teamID);
+    teamName.className = "team-title";
     teamName.innerHTML = document.getElementById("inputNames").value;
     teamBox.appendChild(teamName);
     document.getElementById("inputNames").value = '';
-
 }
 function startDraft(){
     document.getElementById("start-draft").remove();
@@ -289,17 +65,16 @@ function startDraft(){
 }
 function assignDraftOrder(){
     
-    for(i = 1; i<5; i++){
+    for(i = 0; i<4; i++){
         document.getElementById("team"+i).style.opacity = "30%";
     }
 
     showRiders();
-    let teamArray = ['1','2','3','4']
+    let teamArray = ['0','1','2','3']
     
     let randTeam = Math.floor(Math.random()*4);
-    firstDraft = document.getElementById('team'+teamArray[randTeam]);
     currentDraft = teamArray[randTeam];
-
+    firstDraft = document.getElementById('team'+currentDraft);
     firstDraft.style.opacity = "100%";
 
 }
@@ -408,16 +183,8 @@ function highlightRider(riderID, rider){
 
 
 function draft(riderID,rider){
-
-
-
-
     let currentTeam = document.getElementById("team"+currentDraft);
-    let teamLeft = parseInt($(currentTeam).css("left"), 10) + 150;
-    let teamHeight = parseInt($(currentTeam).css("height"),10)+500;
-
-    $(riderCard).animate({left: teamLeft, width: '15%', top: '+=30%'},"fast");
-    $(riderCard).animate({top: teamHeight, opacity: '0%', height: '0px'}, 'fast', function(){$(".riderCard").remove()});
+    $(riderCard).animate({top: '100%', opacity: '0%', height: '0px'}, 'medium', function(){$(".riderCard").remove()});
     
     let addedRider = document.createElement('div');
     addedRider.className = "drafted-rider"
@@ -437,8 +204,8 @@ function draft(riderID,rider){
     //move to next draft
     currentDraft++;
 
-    if(currentDraft > 4){
-        currentDraft = 1;
+    if(currentDraft > 3){
+        currentDraft = 0;
     }
     currentTeam = document.getElementById("team"+currentDraft);
     if(document.getElementById("roster-container").innerHTML == ""){
